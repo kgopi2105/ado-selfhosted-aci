1112ddae232c96fd1a2845965d9243b6d65a9647
kgopi2105 <88634264+kgopi2105@users.noreply.github.com>
2021-08-15 01:32:08 +0100

    Update aci_depend.tf

--- terraform/aci_depend.tf
+++ terraform/aci_depend.tf
@@ -19,7 +19,7 @@ resource "azurerm_key_vault_access_policy" "spkvaccess" {
 
 resource "azurerm_key_vault_access_policy" "userkvaccess" {
   key_vault_id       = azurerm_key_vault.kv.id
-  object_id          = "4de5e895-a0a3-48e7-b3da-1be81f968a96"
+  object_id          = "5534de5e895-a0a3-48e7-b3da-1be8968a96"
   tenant_id          = data.azurerm_client_config.current_config.tenant_id
   secret_permissions = ["Backup", "Delete", "get", "list", "purge", "recover", "restore", "set"]
 }
